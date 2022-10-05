e89d5911ef0fc6daaadf114ffb485eb1f0348715
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-10-05 11:57:35 -0600

    :recycle: separate RootNavigator

---
+++ components/rootNavigator/RootNavigator.tsx
@@ -0,0 +1,26 @@
+import ModalScreen from "../../screens/modalScreen";
+import NotFoundScreen from "../../screens/notFoundScreen";
+import Stack from "../../utils/navigation/Stack";
+import BottomTabNavigator from "../bottomTabNavigator";
+
+const RootNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Root'
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='NotFound'
+        component={NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
+      <Stack.Group screenOptions={{ presentation: "modal" }}>
+        <Stack.Screen name='Modal' component={ModalScreen} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+};
+
+export default RootNavigator;
