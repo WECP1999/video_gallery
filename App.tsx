9f6f7c3bff7bc9d5655cb60d83c79b090026177c
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-10-05 11:59:35 -0600

    :recycle: change root for hooks

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
+import useCachedResources from './utils/hooks/useCachedResources';
+import useColorScheme from './utils/hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
