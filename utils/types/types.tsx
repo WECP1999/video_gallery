9719137cfed884ab166e1d50cfe08739cc5a9a85
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-10-05 10:56:15 -0600

    :truck: move types file to utils directory

--- utils/types/types.tsx
+++ utils/types/types.tsx
@@ -25,8 +25,10 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 >;
 
 export type RootTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Movies: undefined;
+  Series: undefined;
+  ComingSoon: undefined;
+  MovieDetail: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
