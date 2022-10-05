9c7f3b6457e0ebf8464fc969d6f8c13824afc0fb
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-10-05 10:33:30 -0600

    :recycle: move hooks to a global utils directory

---
+++ utils/hooks/useCachedResources.ts
@@ -0,0 +1,30 @@
+import { FontAwesome } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+
+const useCachedResources = () => {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  useEffect(() => {
+    const loadResourcesAndDataAsync = async () => {
+      try {
+        SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          ...FontAwesome.font,
+          "space-mono": require("../../assets/fonts/SpaceMono-Regular.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hideAsync();
+      }
+    };
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return isLoadingComplete;
+};
+
+export default useCachedResources;
